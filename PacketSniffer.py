f7b1d9c5e42f1a384c70d1354f2f929bd9275794
Dave <david.drexler.dd@gmail.com>
2017-11-23 15:01:08 +0000

    1st commmit c9

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -22,5 +22,17 @@ def get_mac_addr(bytes_addr):
 def ethernet_frame(data):
     dest_mac, src_mac, proto = struct.unpack('! 6s 6s H', data[:14])
     return get_mac_addr(dest_mac), get_mac_addr(src_mac), socket.htons(proto), data[14:]
+    
+# Unpack IPv4 packet
+def ipv4_packet(data):
+    version_header_length = data[0]
+    version = version_header_length >> 4
+    header_length = (version_header_length & 15) * 4
+    ttl, proto, src, target = struct.unpack('! 8x B B 2x 4s 4s', data[:20])
+    return version, header_length, ttl, proto, ipv4(src), ipv4(target), data[header_length:]
+
+# Returns properly formatted IPV4 address
+def ipv4(addr):
+    return '.'.join(map(str, addr))
 
 main()
